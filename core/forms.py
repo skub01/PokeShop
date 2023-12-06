d9d9eeee3b6120ca9603a76ee562bfedeaeb6306
Sarah Kublin <127888292+skub01@users.noreply.github.com>
2023-12-06 14:48:02 -0500

    signup form formatting

--- core/forms.py
+++ core/forms.py
@@ -6,3 +6,23 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': "Enter username",
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
+
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'placeholder': "Enter your email",
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
+
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': "Choose a secure password",
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
+
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': "Confirm password",
+        'class': 'w-full py-4 px-6 rounded-xl'
+    }))
