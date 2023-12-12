92131b3cdda1f78fd2cb94d12a926502b94c2188
Sarah Kublin <127888292+skub01@users.noreply.github.com>
2023-12-12 12:57:57 -0500

    render api data on single item page

--- item/views.py
+++ item/views.py
@@ -21,12 +21,14 @@ def items(request):
         items_data = []
         for item_data in data_list:
             name = item_data.get("name", "")
+            id = item_data.get("id", "")
             image = item_data.get("images", {}).get("small", "")
             average_sell_price = item_data.get("cardmarket", {}).get("prices", {}).get("averageSellPrice", 0)
             series = item_data.get("set", {}).get("series", "")
 
             item_info = {
                 'name': name,
+                'id': id,
                 'image': image,
                 'average_sell_price': average_sell_price,
                 'series': series
@@ -34,30 +36,49 @@ def items(request):
 
             items_data.append(item_info)
 
-        paginator = Paginator(items_data, 15)  # Show 15 items per page
+        paginator = Paginator(items_data, 15)  
         page = request.GET.get('page')
 
         try:
             items_data = paginator.page(page)
         except PageNotAnInteger:
-            # If the page parameter is not an integer, deliver the first page.
             items_data = paginator.page(1)
         except EmptyPage:
-            # If the page is out of range (e.g., 9999), deliver the last page.
             items_data = paginator.page(paginator.num_pages)
 
         return render(request, 'item/items.html', {'items_data': items_data, 'paginator': paginator})
     else:
         return render(request, 'item/items.html', {'error_message': 'Failed to fetch data from the API'})
 
-def detail(request, pk):
-    item = get_object_or_404(Item, pk=pk)
-    related_items = Item.objects.filter(category=item.category, sold=False).exclude(pk=pk)[0:3]
+def detail(request, item_id):
+    api_url = f"https://api.pokemontcg.io/v2/cards/{item_id}"
+    response = requests.get(api_url)
 
-    return render(request, 'item/detail.html', {
-        'item': item,
-        'related_items': related_items,
-    })
+    if response.status_code == 200:
+        data = response.json().get("data", {})
+
+        name = data.get("name", "")
+        id = data.get("id", "")
+        image = data.get("images", {}).get("small", "")
+        series = data.get("set", {}).get("series", "")
+        price = data.get("cardmarket", {}).get("prices", {}).get("averageSellPrice", 0)
+        text = data.get("flavorText", "")
+        
+        item_data = {
+            'name': name,
+            'id': id,
+            'image': image,
+            'series': series,
+            'price': price,
+            'text': text
+        }
+
+        return render(request, 'item/detail.html', {
+            'item_data': item_data,
+           })
+    else:
+        return render(request, 'item/detail.html', {'error_message': 'Failed to fetch data from the API'})
+ 
 
 @login_required
 def new(request):
@@ -69,7 +90,7 @@ def new(request):
             item.created_by = request.user
             item.save()
 
-            return redirect('item:detail', pk=item.id)
+            return redirect('item:detail', item_id=item.id)
     else:
         form = NewItemForm()
 
@@ -79,8 +100,8 @@ def new(request):
     })
 
 @login_required
-def edit(request, pk):
-    item = get_object_or_404(Item, pk=pk, created_by= request.user)
+def edit(request, item_id):
+    item = get_object_or_404(Item, id=item_id, created_by= request.user)
 
     if request.method == 'POST':
         form = EditItemForm(request.POST, request.FILES, instance=item)
@@ -88,7 +109,7 @@ def edit(request, pk):
         if form.is_valid():
             form.save()
 
-            return redirect('item:detail', pk=item.id)
+            return redirect('item:detail', item_id=item.id)
     else:
         form = EditItemForm(instance=item)
 
@@ -98,8 +119,8 @@ def edit(request, pk):
     })
 
 @login_required
-def delete(request, pk):
-    item = get_object_or_404(Item, pk=pk, created_by= request.user)
+def delete(request, item_id):
+    item = get_object_or_404(Item, id=item_id, created_by= request.user)
     item.delete()
 
     return redirect('dashboard:index')
