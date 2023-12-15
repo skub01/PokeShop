d0afed59daa24480fbc3f9b15729aea7b1ef14fd
Sarah Kublin <127888292+skub01@users.noreply.github.com>
2023-12-15 09:20:43 -0500

    create terms and privacy pages

--- core/urls.py
+++ core/urls.py
@@ -9,6 +9,8 @@ app_name = 'core'
 urlpatterns = [
     path('', views.index, name='index'),
     path('contact/', views.contact, name='contact'),
+    path('privacy/', views.privacy, name='privacy'),
+    path('terms/', views.terms, name='terms'),
     path('signup/', views.signup, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=LoginForm), name='login'),
 ]
--- core/views.py
+++ core/views.py
@@ -14,6 +14,12 @@ def index(request):
 def contact(request):
     return render(request, 'core/contact.html')
 
+def privacy(request):
+    return render(request, 'core/privacy.html')
+
+def terms(request):
+    return render(request, 'core/terms.html')
+
 def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
