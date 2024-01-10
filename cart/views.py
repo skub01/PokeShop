bfd9f181aedf43459cac4db2631c28fa3541b732
Sarah Kublin <127888292+skub01@users.noreply.github.com>
2024-01-10 17:16:36 -0500

    create cart views.py

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Cart, CartItem
+from item.models import Item
+from django.contrib import messages
 
-# Create your views here.
+def view_cart(request):
+    cart_items = CartItem.objects.filter(cart__user=request.user)
+    total_price = sum(item.quantity * item.item.price for item in cart_items)
+
+    return render(request, 'cart/cart_view.html', {'cart_items': cart_items, 'total_price': total_price})
+
+def delete_item(request, item_id):
+    item = get_object_or_404(CartItem, id=item_id)
+    item.delete()
+    messages.success(request, 'Item removed from cart.')
+    return redirect('cart:view_cart')
