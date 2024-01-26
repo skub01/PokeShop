d9655f47d9a872b5f6666548345f430f3c539238
Sarah Kublin <127888292+skub01@users.noreply.github.com>
2024-01-26 15:49:39 -0500

    create process_payment function

--- cart/models.py
+++ cart/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 from item.models import Item
+from django.shortcuts import render
+
+def process_payment(request):
+    user_cart = Cart.objects.get(user=request.user)
+    cart_items = user_cart.items.all()
+    total_price = sum(item.item.price * item.quantity for item in cart_items)
+
+    user_cart.items.all().delete()
+
+    return render(request, 'checkout/order.html', {'ordered_items': cart_items, 'total_price': total_price})
 
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
