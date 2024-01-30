6a19377a9e3997bfc9afb35a3e0a615184197882
Sarah Kublin <127888292+skub01@users.noreply.github.com>
2024-01-29 20:10:53 -0500

    def checkout and order

--- cart/views.py
+++ cart/views.py
@@ -35,3 +35,18 @@ def process_payment(request):
     user_cart.items.all().delete()
 
     return render(request, 'checkout/order.html', {'ordered_items': cart_items, 'total_price': total_price})
+
+def checkout(request):
+    cart_items = CartItem.objects.filter(cart__user=request.user)
+    total_price = sum(item.quantity * item.item.price for item in cart_items)
+
+    return render(request, 'cart/checkout.html', {'cart_items': cart_items, 'total_price': total_price})
+
+def order(request, order_id):
+    ordered_items = CartItem.objects.filter(order__id=order_id)
+    total_price = sum(item.quantity * item.item.price for item in ordered_items)
+
+    context = {
+        'ordered_items': ordered_items,
+        'total_price': total_price,
+    }
