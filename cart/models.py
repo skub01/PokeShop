67f542a059e549eed22e035f7d6d5b8597b722c4
Sarah Kublin <127888292+skub01@users.noreply.github.com>
2024-01-30 19:39:55 -0500

    add order model

--- cart/models.py
+++ cart/models.py
@@ -3,6 +3,13 @@ from django.contrib.auth.models import User
 from item.models import Item
 from django.shortcuts import render
 
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order #{self.id} for {self.user.username}"
+
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
