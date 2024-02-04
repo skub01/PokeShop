1f39fa499c3e03578f49d5c69e4bcacb4f7cbe4b
Sarah Kublin <127888292+skub01@users.noreply.github.com>
2024-02-04 16:51:09 -0500

    require login for cart

--- cart/views.py
+++ cart/views.py
@@ -2,19 +2,23 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Cart, CartItem
 from item.models import Item
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def view_cart(request):
     cart_items = CartItem.objects.filter(cart__user=request.user)
     total_price = sum(item.quantity * item.item.price for item in cart_items)
 
     return render(request, 'cart/cart_view.html', {'cart_items': cart_items, 'total_price': total_price})
 
+@login_required
 def delete_item(request, item_id):
     item = get_object_or_404(CartItem, id=item_id)
     item.delete()
     messages.success(request, 'Item removed from cart.')
     return redirect('cart:view_cart')
 
+@login_required
 def add_to_cart(request, item_id):
     item = get_object_or_404(Item, id=item_id)
     user_cart, created = Cart.objects.get_or_create(user=request.user)
@@ -27,6 +31,7 @@ def add_to_cart(request, item_id):
 
     return redirect('cart:view_cart')
 
+@login_required
 def process_payment(request):
     user_cart = Cart.objects.get(user=request.user)
     cart_items = user_cart.items.all()
@@ -36,12 +41,14 @@ def process_payment(request):
 
     return render(request, 'checkout/order.html', {'ordered_items': cart_items, 'total_price': total_price})
 
+@login_required
 def checkout(request):
     cart_items = CartItem.objects.filter(cart__user=request.user)
     total_price = sum(item.quantity * item.item.price for item in cart_items)
 
     return render(request, 'cart/checkout.html', {'cart_items': cart_items, 'total_price': total_price})
 
+@login_required
 def order(request, order_id):
     ordered_items = CartItem.objects.filter(order__id=order_id)
     total_price = sum(item.quantity * item.item.price for item in ordered_items)
@@ -50,5 +57,5 @@ def order(request, order_id):
         'ordered_items': ordered_items,
         'total_price': total_price,
     }
-    
+
     return render(request, 'checkout/order.html', context)
